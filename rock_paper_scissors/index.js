99143451056a3458befc568f7ffb124820798380
Ian <ianmat55@gmail.com>
2021-11-03 10:52:49 -0700

    rock paper scissors challenge added

---
+++ rock_paper_scissors/index.js
@@ -0,0 +1,11 @@
+// User selects rock, rock paper or scissors. Computer selects rock, paper or scissors. 
+// Keeps track of wins/losses
+// cool animation
+
+const playerSelect = () => {
+
+};
+
+const computerSelect = () => {
+
+};

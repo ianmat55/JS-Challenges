289eb8891a879fc70d6083b203cb4e785769a988
Ian <ianmat55@gmail.com>
2021-11-03 20:19:43 -0700

    added reset score button

--- rock_paper_scissors/index.js
+++ rock_paper_scissors/index.js
@@ -2,26 +2,41 @@
 // Keeps track of wins/losses
 // cool animation
 
+let playerCount = 0;
+let compCount = 0;
+const jonKenPo = ['rock', 'paper', 'scissors']
+
 const getRandomInt = (n) => {
 	return Math.floor(Math.random() * n);
 }
-
-const playerSelect = () => {
-	let player = window.prompt("rock, paper, scissors");
-	return player;
-};
 
 const computerSelect = () => {
 	let comp = getRandomInt(3);
 	return comp;
 };
 
+const updateScore = (winner) => {
+	let update = document.getElementById(winner);
+
+	if (winner=='player') {
+		playerCount++;
+		update.innerHTML = playerCount;
+	} else if (winner=='computer') {
+		compCount++
+		update.innerHTML = compCount;
+	}
+};
+
+const resetScore = () => {
+	let resetP = document.getElementById('player');
+	resetP.innerHTML = 0;
+	let resetC = document.getElementById('computer');
+	resetC.innerHTML = 0;
+}
+
 const playGame = async(p) => {
 	const results = ['Its a tie!', 'Player wins', 'Computer wins'];
-	const jonKenPo = ['rock', 'paper', 'scissors']
 	let message;
-	let playerCount = 0;
-	let compCount = 0;
 	let c = computerSelect();
 
 	// logic
@@ -29,22 +44,22 @@ const playGame = async(p) => {
 		message = results[0];
 	} else if (p>c & c==0 & p!=1) {
 		message = results[2];
-		compCount++;
+		updateScore('computer');
 	} else if (p<c & p==0 & c!=1) {
 		message = results[1];
-		playerCount++;
+		updateScore('player');
 	} else if (p<c & c==1) {
 		message = results[2];
-		compCount++;
+		updateScore('computer');
 	} else if (p>c & p==1) {
 		message = results[1];
-		playerCount++;
+		updateScore('player');
 	} else if (p>c) {
 		message = results[1];
-		playerCount++;
+		updateScore('player');
 	} else if (c>p) {
 		message = results[2];
-		compCount++;
+		updateScore('computer');
 	}
 
 	alert(`player: ${jonKenPo[p]}, computer: ${jonKenPo[c]}\n${message}`);

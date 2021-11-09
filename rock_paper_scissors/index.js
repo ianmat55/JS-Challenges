990f9be23ab518edd0d732c9bb2f88c641f048b9
Ian <ianmat55@gmail.com>
2021-11-09 08:08:51 -0800

    added animation to rpc game

--- rock_paper_scissors/index.js
+++ rock_paper_scissors/index.js
@@ -32,7 +32,25 @@ const resetScore = () => {
 	resetP.innerHTML = 0;
 	let resetC = document.getElementById('computer');
 	resetC.innerHTML = 0;
-}
+};
+
+const displayChoice = (p, c) => {
+	const rpc = ['../images/rock.png', '../images/paper.png', '../images/scissors.png']
+	const left = document.querySelector('#player-choice');
+	const right = document.querySelector('#comp-choice');
+	left.src = rpc[p];
+	right.src = rpc[c];
+
+	const displayWinner = document.querySelector('#winner-msg');
+	
+	displayWinner.innerHTML = `<h1> player: ${jonKenPo[p]}, computer: ${jonKenPo[c]} </h1>`
+
+	setTimeout(function() {
+		left.src = rpc[0];
+		right.src = rpc[0];
+		// displayWinner.removeChild('h1');
+	}, 2000);
+};
 
 const playGame = async(p) => {
 	const results = ['Its a tie!', 'Player wins', 'Computer wins'];
@@ -42,27 +60,40 @@ const playGame = async(p) => {
 	// logic
 	if (p==c) {
 		message = results[0];
-	} else if (p>c & c==0 & p!=1) {
+		if (p==0 & c==0) {
+			displayChoice(0, 0);
+		} else if (p==1 & c==1) {
+			displayChoice(1, 1);
+		} else {
+			displayChoice(2, 2);
+		}
+	} else if (p>c & c==0 & p!=1) { // scissors -- rock
 		message = results[2];
 		updateScore('computer');
-	} else if (p<c & p==0 & c!=1) {
+		displayChoice(2,0) 
+	} else if (p<c & p==0 & c!=1) { // rock -- scissors
 		message = results[1];
 		updateScore('player');
-	} else if (p<c & c==1) {
+		displayChoice(0,2)
+	} else if (p<c & c==1) { // rock -- paper
 		message = results[2];
 		updateScore('computer');
-	} else if (p>c & p==1) {
+		displayChoice(0,1)
+	} else if (p>c & p==1) { // paper -- rock
 		message = results[1];
 		updateScore('player');
-	} else if (p>c) {
+		displayChoice(1,0)
+	} else if (p>c) { // scissors -- paper
 		message = results[1];
 		updateScore('player');
-	} else if (c>p) {
+		displayChoice(2,1)
+	} else if (c>p) { // paper -- scissors
 		message = results[2];
 		updateScore('computer');
+		displayChoice(1,2)
 	}
 
-	alert(`player: ${jonKenPo[p]}, computer: ${jonKenPo[c]}\n${message}`);
+	// alert(`player: ${jonKenPo[p]}, computer: ${jonKenPo[c]}\n${message}`);
 };
 
 const rock = document.querySelector('#rock');
@@ -85,3 +116,4 @@ scissors.addEventListener('click', () => {
 reset.addEventListener('click', () => {
 	resetScore();
 })
+

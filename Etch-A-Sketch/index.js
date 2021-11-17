400b6feaa89bd7bb11a0e465be5b3bf890eafbd2
ianmat55 <ianmat55@gmail.com>
2021-11-16 23:08:58 -0800

    grid redraw is very buggy, also the settings section pushes up on re render

--- Etch-A-Sketch/index.js
+++ Etch-A-Sketch/index.js
@@ -1,19 +1,26 @@
 // populating html purely with js
 const title = document.createElement('h1');
+const directions = document.createElement('h3');
 const body = document.querySelector('body');
+const gridSection = document.createElement('div');
 const gridDiv = document.createElement('div');
 const settings = document.createElement('div');
+const btnDropDown = document.createElement('div');
 
 // TITLE
 const addTitle = () => {
 	title.textContent = 'Etch-a-Sketch';
 	body.appendChild(title);
+	directions.textContent = 'Hold shift and hover to draw';
+	body.appendChild(directions);
 }
 
 // GRID
 const renderGrid = (col, row) => {
+	gridSection.id = 'section';
 	gridDiv.id = 'grid';
-	body.appendChild(gridDiv);
+	body.appendChild(gridSection);
+	gridSection.appendChild(gridDiv);
 	
 	for(let i=0; i<(col*row); i++) {
 		const gridBox = document.createElement('div');
@@ -26,8 +33,9 @@ const renderGrid = (col, row) => {
 
 // BUTTONS
 const renderButtons = () => {
-	const btns = ['resetGrid', 'setDimensions'];
-	const btnNames = ['Reset', 'Set Dimensions']
+	const btns = ['resetGrid', 'setDimensions', 'rgb', 'grayScale'];
+	const btnNames = ['Reset', 'Set Dimensions', 'RGB', 'Gray-Scale'];
+
 	for (let i=0; i<btns.length; i++) {
 		let temp = document.createElement('button');
 		temp.innerText = btnNames[i]
@@ -37,14 +45,27 @@ const renderButtons = () => {
 		temp.width = '200px';
 		settings.appendChild(temp);
 	}
+
 	settings.id = 'settings';
 	body.appendChild(settings);
+
+	// render grid button dropdown form
+	// const form = document.createElement('form');
+	// const inputCol = document.createElement('input');
+	// const inputRow = document.createElement('input');
+	// const submit = document.createElement('submit');
+	// form.appendChild(inputCol);
+	// form.appendChild(inputRow);
+	// form.append(submit);
+	// btnDropDown.appendChild(form);
+	// btnDropDown.style.display = 'none';
 }
 
 // MAIN 
 const main = async() => {
 	let col = 16; //default
 	let row = 16; //default
+	let lineColor = 'blue';
 
 	try{
 		await addTitle();
@@ -55,7 +76,7 @@ const main = async() => {
 		const gridSpaces = document.querySelectorAll('.grid-space');
 		gridSpaces.forEach(grid => grid.addEventListener('mouseover', (e) => {
 		if (e.shiftKey) {
-			grid.style.background = 'blue';
+			grid.style.background = lineColor;
 			}
 		}));
 
@@ -67,9 +88,18 @@ const main = async() => {
 
 		// form for new grid with dimensions col and row, limit 100
 		const setDimensions = document.querySelector('#setDimensions');
-		setDimensions.addEventListener('click', () => {
-			alert(input('Columns:'));
-			alert(input('Rows:'));
+		setDimensions.addEventListener('click', async() => {
+			let input = prompt('Enter Number for a x by x grid');
+			await gridDiv.remove();
+			if (parseInt(input) > 100 || parseInt(input) <= 0 ) {
+				alert('Number must between 1 and 100');
+			} else if (!parseInt(input)) {
+				alert('Please enter a number');
+			} else {
+				col = row = Math.floor(input);
+				alert(`${col} and ${row}`);
+				renderGrid(col, row);
+			}
 		});
 
 		// rgb input value for square color

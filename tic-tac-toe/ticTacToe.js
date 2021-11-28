191c570d6c17b999fa8f35ef6d4cf7eb2c823b18
ianmat55 <ianmat55@gmail.com>
2021-11-27 20:24:25 -0800

    game logic added to tic tac toe, no logic to test for winners, only clear board and input symbol

--- tic-tac-toe/ticTacToe.js
+++ tic-tac-toe/ticTacToe.js
@@ -47,30 +47,146 @@ const loadHTML = (() => {
 		settingsDiv.appendChild(pvp);
 
 		aiBtn.addEventListener('click', async() => {
+			const player1 = player('X');
+			const bot = unbeatableComputer('O');
+
 			await settingsDiv.remove();
+			await loadGameBoard(player1, bot);
 			await displayController();
 		});
 	
 		pvp.addEventListener('click', async() => {
+			const player1 = player('X');
+			const player2 = player('O');
+
 			await settingsDiv.remove();
+			await loadGameBoard(player1, player2);
 			await displayController();
 		})
 	});
 })();
 
 // IIFE since there is only one gameboard
-const loadGameBoard = () => {
-	const gameBoard = [];
+const loadGameBoard = (p1, p2) => {
 	const container = document.querySelector('.container');
-	const gameDiv = document.createElement('div');
-	gameDiv.setAttribute('id', 'gameBoard');
-	container.append(gameDiv);
 
-	for (let i=0; i<9; i++) {
-		const gridSquare = document.createElement('div');
-		gridSquare.classList.add('gridSquare');
-		gameDiv.appendChild(gridSquare);
-	}
+	// display player/bot cards
+	const displayCards = (() => {
+		const card1 = p1.displayCard();
+		const card2 = p2.displayCard();
+
+		const playerCards = document.createElement('div');
+		playerCards.setAttribute('id', 'playerCards');
+		const player1 = document.createElement('div');
+		const player2 = document.createElement('div');
+
+		playerSymbol1 = document.createElement('h2');
+		playerSymbol1.innerHTML = p1.getSign();
+		player1.appendChild(playerSymbol1);
+		playerName1 = document.createElement('h3');
+		playerName1.innerHTML = `Player ${card1}`;
+		player1.appendChild(playerName1);
+		player1.classList.add('player');
+		playerCards.appendChild(player1);
+
+		playerName2 = document.createElement('h3');
+		if (card2 != 'Robot') {
+			playerName2.innerHTML = `Player ${card2}`
+			player2.classList.add('player');
+		} else {
+			playerName2.innerHTML = card2;
+			player2.classList.add('bot')
+		}
+		playerSymbol2 = document.createElement('h2');
+		playerSymbol2.innerHTML = p2.getSign();
+		player2.appendChild(playerSymbol2);
+		player2.appendChild(playerName2);
+		playerCards.appendChild(player2);
+
+		container.appendChild(playerCards);
+	})();
+
+	// load game board
+	const loadBoard = (() => {
+		const gameDiv = document.createElement('div');
+		gameDiv.setAttribute('id', 'gameBoard');
+		container.appendChild(gameDiv);
+
+		for (let i=0; i<9; i++) {
+			const gridSquare = document.createElement('div');
+			gridSquare.classList.add('gridSquare');
+			gameDiv.appendChild(gridSquare);
+		}
+	})();
+
+	// load reset button
+	const resetButton = (() => {	
+		const resetDisplay = document.createElement('div');
+		resetDisplay.setAttribute('id', 'resetSettings');
+		container.appendChild(resetDisplay);
+		const resetButton = document.createElement('button');
+		resetButton.classList.add('btn', 'btn-danger');
+		resetButton.innerHTML = 'Reset';
+		resetDisplay.appendChild(resetButton);
+	})();
+
+	return { displayCards, loadBoard, resetButton };
+};
+
+
+// Where the game logic happens
+const displayController = () => {
+	let grid = [
+		'', '', '',
+		'', '', '',
+		'', '', ''
+	];
+
+	/*
+		[0] [1] [2]
+		[3] [4] [5]
+		[6] [7] [8]
+	*/
+
+	const winningConditions = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[2, 4, 6],
+		[0, 4, 1]
+	];
+	let currentPlayer = 'X';
+	let gameActive = true;
+	const cells = document.querySelectorAll('.gridSquare');
+	const resetBtn = document.querySelector('.btn-danger');
+
+	// How does forEach know what to assign each parameter? How does it magically know the index?
+	cells.forEach((cell, index) => {
+		cell.addEventListener('click', () => displayMove(cell, index));
+	});
+
+	resetBtn.addEventListener('click', () => clear());
+
+	const displayMove = (cell, index) => {
+		if (grid[index] != '') {
+			return
+		}
+		cell.innerHTML = currentPlayer;
+		grid[index] = currentPlayer;
+
+		if (currentPlayer == 'X') {
+			// set color to green and change currentPlayer to O
+			cell.classList.add('green');
+			currentPlayer = 'O';
+		} else if (currentPlayer == 'O') {
+			// set color to pink and change currentPlayer to X
+			cell.classList.add('pink');
+			currentPlayer = 'X';
+		}
+	};
 
 	const clear = () => {
 		for (let i=0; i<gameBoard.length; i++) {
@@ -79,32 +195,36 @@ const loadGameBoard = () => {
 
 		const cells = document.querySelectorAll('.gridSquare');
 		cells.forEach(cell => {
-			// reset display logic
+			cell.innerHTML = "";
+			cell.className = 'gridSquare';
 		});
 	}
-
-	// const setMove
 
 	return { clear };
 };
 
-const displayController = async() => {
-	await loadGameBoard();
-	const cells = document.querySelectorAll('.gridSquare');
-	cells.forEach(cell => {
-		cell.addEventListener('click', () => {console.log('idk')});
-	});
-
-	// checks for wins
-};
-
-
 const player = (sign) => {
 	const getSign = () => sign;
 
-	return { getSign }
+	const getRandom = (max) => {
+		return Math.floor(Math.random() * max)
+	}
+
+	const displayCard = () => {
+		const numberOfPlayers = getRandom(456) // number of squid game players
+
+		return numberOfPlayers
+	}
+
+	return { getSign, displayCard }
 };
 
-const UnbeatableComputer = (() => {
+const unbeatableComputer = (sign) => {
+	const getSign = () => sign;
+	
+	const displayCard = () => {
+		return 'Robot'
+	}
 
-});
+	return { displayCard, getSign }
+};

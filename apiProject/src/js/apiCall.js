1d370431224cb54f35097ad898a1bc14dfcf0309
ianmat55 <ianmat55@gmail.com>
2022-01-25 10:50:42 -0800

    removed openweather project

--- apiProject/src/js/apiCall.js
+++
@@ -1,8 +0,0 @@
-// process.env.apiKEY
-async function getWeather(city, key) {
-	const call = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
-	const response = await fetch(call);
-	return response.json();
-}
-
-export { getWeather }
